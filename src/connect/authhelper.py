637b3f18dd37d02a33ea2e3aeea4eb14d58ebe9f
4k45hv3rm4 <4k45hr0ck5007@gmail.com>
2020-02-04 23:18:45 +0530

    added authentication helper function

--- src/connect/authhelper.py
+++ src/connect/authhelper.py
@@ -1,76 +1,99 @@
 from urllib.parse import quote, urlencode
+import requests
 import base64
 import json
 import time
-import requests
 
+# Client ID and secret
 client_id     = "293d9590-b9a9-44aa-80f5-1c1bcc6229e2"
 client_secret = "Zdv.Zl7PyElmsQGU4K:rUu:bq[kx4pv6"
 
-authority     = 'https://login.microsoftonline.com'
+# Constant strings for OAuth2 flow
+# The OAuth authority
+authority = 'https://login.microsoftonline.com'
 
+# The authorize URL that initiates the OAuth2 client credential flow for admin consent
 authorize_url = '{0}{1}'.format(authority, '/common/oauth2/v2.0/authorize?{0}')
 
-token_url     = '{0}{1}'.format(authority, '/common/oauth2/v2.0/token')
+# The token issuing endpoint
+token_url = '{0}{1}'.format(authority, '/common/oauth2/v2.0/token')
 
-
+# The scopes required by the app
 scopes = [ 'openid',
            'offline_access',
            'User.Read',
-           'Mail.Read' ]
+           'Mail.Read',
+           'Calendars.Read',
+           'Contacts.Read' ]
 
-def get_signin_url(redirect_url):
-
-    params = { 'client_id': client_id,
-             'redirect_url': redirect_url,
+def get_signin_url(redirect_uri):
+  # Build the query parameters for the signin url
+  params = { 'client_id': client_id,
+             'redirect_uri': redirect_uri,
              'response_type': 'code',
              'scope': ' '.join(str(i) for i in scopes)
-            }
+           }
 
-    signin_url = authorize_url.format(urlencode(params))
+  signin_url = authorize_url.format(urlencode(params))
 
-    return signin_url
+  return signin_url
 
 def get_token_from_code(auth_code, redirect_uri):
-    post_data = {'grant_type'   : 'authorization_code',
-                 'code'         : auth_code,
-                 'redirect_uri' : redirect_uri,
-                 'scope'        : ' '.join(str(i) for i in scopes),
-                 'client_id'    : client_id,
-                 'client_secret': client_secret,
-            }
-    r = requests.post(token_url, data = post_data)
-    try :
-        return r.json()
-    except:
-        return "Error retrieving token :{0} - {1}".format(r.status_code, r.text)
+  # Build the post form for the token request
+  post_data = { 'grant_type': 'authorization_code',
+                'code': auth_code,
+                'redirect_uri': redirect_uri,
+                'scope': ' '.join(str(i) for i in scopes),
+                'client_id': client_id,
+                'client_secret': client_secret
+              }
 
-def get_token_from_refresh_token(refresh_token, redirect_url):
-    post_data = {'grant_type':'refresh_token',
-                 'refresh_token':refresh_token,
-                 'redirect_url':redirect_url,
-                 'scope':' '.join(str(i) for i in species),
-                 'client_id':client_id,
-                 'client_secret':client_secret
-            }
-    r = request.post(token_url, data = post_data)
-    try:
-        return r.json()
-    except:
-        return "Error retrieving token :{0} - {1}".format(r.status_code, r.text)
+  r = requests.post(token_url, data = post_data)
 
-def get_acces_token(request, redirect_url):
-    current_token = request.session['access_token']
-    expiration    = request.session['token_expires']
-    now = int(time.time())
-    if (current_token and  now < expiration):
-        return current_token
-    else:
-        refresh_token = request.session['refresh_token']
-        new_tokens    = get_token_from_refresh_token(refresh_token)
+  try:
+    return r.json()
+  except:
+    return 'Error retrieving token: {0} - {1}'.format(r.status_code, r.text)
 
-        expiration = int(time.time()) + new_tokens['expires_in'] - 300
-        request.session['access_token']  = new_tokens['access_token']
-        request.session['refresh_token'] = new_tokens['refresh_token']
-        request.session['token_expires'] = expiration
-        return new_tokens['access_token']
+def get_token_from_refresh_token(refresh_token, redirect_uri):
+  # Build the post form for the token request
+  post_data = { 'grant_type': 'refresh_token',
+                'refresh_token': refresh_token,
+                'redirect_uri': redirect_uri,
+                'scope': ' '.join(str(i) for i in scopes),
+                'client_id': client_id,
+                'client_secret': client_secret
+              }
+
+  r = requests.post(token_url, data = post_data)
+
+  try:
+    return r.json()
+  except:
+    return 'Error retrieving token: {0} - {1}'.format(r.status_code, r.text)
+
+def get_access_token(request, redirect_uri):
+  current_token = request.session['access_token']
+  expiration = request.session['token_expires']
+  now = int(time.time())
+  if (current_token and now < expiration):
+    # Token still valid
+    return current_token
+  else:
+    # Token expired
+    refresh_token = request.session['refresh_token']
+    new_tokens = get_token_from_refresh_token(refresh_token, redirect_uri)
+
+    # Update session
+    # expires_in is in seconds
+    # Get current timestamp (seconds since Unix Epoch) and
+    # add expires_in to get expiration time
+    # Subtract 5 minutes to allow for clock differences
+    expiration = int(time.time()) + new_tokens['expires_in'] - 300
+
+    # Save the token in the session
+    request.session['access_token'] = new_tokens['access_token']
+    request.session['refresh_token'] = new_tokens['refresh_token']
+    request.session['token_expires'] = expiration
+
+    return new_tokens['access_token']
