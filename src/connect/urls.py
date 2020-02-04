c2c1e83a9241150b4a6b25a7dfe7cf84bd2a1883
4k45hv3rm4 <4k45hr0ck5007@gmail.com>
2020-02-04 23:27:07 +0530

    added urls to the events and contacts"

--- src/connect/urls.py
+++ src/connect/urls.py
@@ -6,5 +6,12 @@ app_name="connect"
 urlpatterns=[
     url(r'^$'         , views.home    , name='home'),
     url(r'^home/$'    , views.home    , name='home'),
-    url(r'^gettoken/$', views.gettoken, name="gettoken"),
+    url(r'^gettoken/$', views.gettoken, name='gettoken'),
+    url(r'^mail/$'    , views.mail    , name="mail"),
+    url(r'^events/$'  , views.events  , name='events'),
+    url(r'^contacts/$', views.contacts, name='contacts'),
 ]
+# https://login.microsoftonline.com/common/oauth2/v2.0/authorize?
+# scope=openid+User.Read+Mail.Read&response_type=code&
+# client_id=293d9590-b9a9-44aa-80f5-1c1bcc6229e2&
+# redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fconnect%2Fgettoken%2F
